38fb67d0d0eaef85214bcf5f1616c2a3258577df
Okappa <48796514+LukasBrauschHFU@users.noreply.github.com>
2020-06-30 12:52:51 +0200

    Add files via upload

--- Calculator/modules/operations.js
+++ Calculator/modules/operations.js
@@ -1,34 +1,24 @@
-System.register([], function (exports_1, context_1) {
-    "use strict";
-    var Operations;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [],
-        execute: function () {
-            Operations = class Operations {
-                static Power(a, exp) {
-                    let result = 1;
-                    for (let i = 0; (i < exp); i++) {
-                        result = (result * a);
-                    }
-                    return result;
-                }
-                static GreatestCommonDenominator(a, b) {
-                    if ((a < b)) {
-                        let tmp = a;
-                        a = b;
-                        b = tmp;
-                    }
-                    while ((b > 0)) {
-                        let c = (a % b);
-                        a = b;
-                        b = c;
-                    }
-                    return a;
-                }
-            };
-            exports_1("default", Operations);
+class Operations {
+    static Power(a, exp) {
+        let result = 1;
+        for (let i = 0; (i < exp); i++) {
+            result = (result * a);
         }
-    };
-});
+        return result;
+    }
+    static GreatestCommonDenominator(a, b) {
+        if ((a < b)) {
+            let tmp = a;
+            a = b;
+            b = tmp;
+        }
+        while ((b > 0)) {
+            let c = (a % b);
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+}
+export default Operations;
 //# sourceMappingURL=operations.js.map
--- Calculator/test.js
+++ Calculator/test.js
@@ -1,18 +1,5 @@
-System.register(["./modules/main.js"], function (exports_1, context_1) {
-    "use strict";
-    var main_js_1, c;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [
-            function (main_js_1_1) {
-                main_js_1 = main_js_1_1;
-            }
-        ],
-        execute: function () {
-            c = new main_js_1.Calculator();
-            main_js_1.test(c, "1+2*33/11=");
-            console.log(main_js_1.test);
-        }
-    };
-});
+import { Calculator, test } from "./modules/main.js";
+let c = new Calculator();
+test(c, "1+2*33/11=");
+console.log(test);
 //# sourceMappingURL=test.js.map
